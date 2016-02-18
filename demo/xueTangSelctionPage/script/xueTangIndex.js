c80d08096d2db9010484fe33e9c1d9f797fd2f98
xiaonongshishui <xiaonongwork@126.com>
2016-02-18 13:29:25 +0800

    111

--- demo/xueTangSelctionPage/script/xueTangIndex.js
+++ demo/xueTangSelctionPage/script/xueTangIndex.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){			
-		$('i').click(function(){
-			var $option = $(this);
+		$('i.select').click(function(){
+			var option = this;
 		// 选择数值层出现
 		$('.selectValue').show();
 		$('.selectValue header h3').text($(this).parents('li').children('strong').text());
@@ -16,15 +16,15 @@ $(document).ready(function(){
 		    scrollbarSnapOnRelease:'true' , });
 		//点击确定
 			$('.selectValue a[title="confirm"]').click(function(event){
-			$option.prev().text($('.swiper-slide-active').text());
+			$(option).prev().text($('.swiper-slide-active').text());
 			$('.selectValue').hide();
 			event.preventDefault();
-			
+			option = null;
 		});
 			$('.selectValue a[title="cancle"]').click(function(event){
 				$('.selectValue').hide();
 				event.preventDefault();
-				$option = null;
+				option = null;
 			});
 
 		});
