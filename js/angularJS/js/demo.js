579b137925282b327e46f3814777ab7baa0efb1d
helurong <helurong@fumi88.com>
2016-06-13 12:03:27 +0800

    啦啦啦

--- js/angularJS/js/demo.js
+++ js/angularJS/js/demo.js
@@ -47,4 +47,73 @@
 			}
 		};
 	});
+	app.controller("MainController",["$scope",function($scope){
+
+	}]);
+	app.directive("myDirective",function(){
+		return {
+			restrict:"A",
+			transclude:true,
+			// scope:true,
+			priority:100,
+			template:"<div>inside myDirective template {{myProperty}}</div><div ng-transclude></div>"
+		}
+	});
+	app.controller("loadDataCtrl",["$scope","$http",function($scope,$http){
+		$scope.users = [
+		{
+			name:"小明"
+		},{
+			name:"小红"
+		},{
+			name:"小强"
+		},{
+			name:"小美"
+		},{
+			name:"小丽"
+		},{
+			name:"小何"
+		}];
+	}];
+	// $http({
+	// 	method:"GET",
+	// 	url:"data.json"
+	// }).success(function(data,status,headers,config){
+	// 	console.log(success...);
+	// 	console.log(data);
+	// 	$scope.users = data;
+	// }).error(function(data,status,headers,config){
+	// 	console.log("error...");
+	// });
+	);
+	app.factory("userListService",["$http",function($http){
+		var doRequest = function(uesername,path){
+			return $http({
+				method:"GET",
+				url:"users.json"
+			});
+		}
+		return {
+			userList:function(username){
+				return doRequest(username,"userList");
+			};
+		}
+	}]);
+	app.controller("ServiceController",["$scope","$timeout","userListService",
+		function($scope,$timeout,userListService){
+			var timeout;
+			$scope.$watch("username",function(newUserName){
+				if(newUserName){
+					if(timeout){
+						$timeout.cancel(timeout);
+					}
+					timeout = $timeout(function(){
+						userListService.userList(newUserName)
+						.success(function(data,status){
+							$scope.users = data;
+						});
+					},350);
+				}
+			});
+	}]);
 })()
